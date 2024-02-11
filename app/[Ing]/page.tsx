d54f65134d2b3815b4e4909b237abe61728193ee
kypdev <kyptdev@gmail.com>
2024-02-11 17:14:36 +0700

    Done home content register

--- app/[Ing]/page.tsx
+++ app/[Ing]/page.tsx
@@ -2,16 +2,16 @@
 import { Trans } from 'react-i18next/TransWithoutContext'
 import { languages, fallbackLng } from '../i18n/settings'
 import { useTranslation } from '../i18n'
-import { Carousel, Divider } from 'antd'
 import { useEffect, useState } from 'react'
 import Image from 'next/image'
+import { Carousel } from 'react-responsive-carousel'
+import 'react-responsive-carousel/lib/styles/carousel.min.css'
+import { Button, Flex, Input, Typography } from 'antd'
+const { Title, Text } = Typography
 
+const images = ['/images/main.png', '/images/main.png', '/images/main.png']
 
-export default function Home({
-  params: { lng },
-}: {
-  params: { lng: string }
-}) {
+export default function Home({ params: { lng } }: { params: { lng: string } }) {
   if (languages.indexOf(lng) < 0) lng = fallbackLng
   // const { t } = await useTranslation(lng)
   const [padding, setPadding] = useState(false)
@@ -21,48 +21,49 @@ export default function Home({
 
   return (
     <>
-      {padding && (
-        <div className='w-3/5 h-full bg-primary-blue'>
-          <Carousel className=''>
-            <div>
-              {/* <h1
-                style={{
-                  color: 'white',
-                  lineHeight: '300px',
-                  textAlign: 'center',
-                }}
-              >
-                Slide 1
-              </h1> */}
-              <Image src={'/images/main.png'} className='max-w-7xl max-h-96' alt={''} width={1280} height={359}/>
+      <div className='w-3/5 h-full'>
+        <Carousel useKeyboardArrows={true} showThumbs={false}>
+          {images.map((URL, index) => (
+            <div key={index} className='slide'>
+              <img alt='sample_file' src={URL} key={index} />
             </div>
-            <div>
-              <h1
-                style={{
-                  color: 'white',
-                  lineHeight: '300px',
-                  textAlign: 'center',
-                }}
+          ))}
+        </Carousel>
+        <div className=' bg-primary-green px-10 py-10 my-10'>main content</div>
+        <div className='flex flex-row  bg-primary-green px-40 py-10  my-10 justify-between'>
+          <div className='flex flex-col'>
+            <h1 className='text-4xl font-bold text-white'>สมัครสมาชิก</h1>
+            <p className='text-white text-lg'>
+              สมัครสมาชิกเพื่อรับบริการจอดรถยนต์กับ ParkX
+            </p>
+            <div className='flex flex-row justify-between px-4'>
+              <Button
+                className='bg-primary-blue text-white hover:bg-primary-blue px-4'
+                shape='round'
               >
-                Slide 2
-              </h1>
-            </div>
-            <div>
-              <h1
-                style={{
-                  color: 'white',
-                  lineHeight: '300px',
-                  textAlign: 'center',
-                }}
+                signin
+              </Button>
+              <Button
+                className='bg-secondary-blue text-white hover:bg-secondary-blue px-10'
+                shape='round'
               >
-                Slide 3
-              </h1>
+                signup
+              </Button>
             </div>
-          </Carousel>
+          </div>
+          <div className='flex flex-col justify-center'>
+            <p className='text-white text-lg'>ติดตามข่าวสารและโปรโมชัน</p>
+            <div className='flex flex-row'>
+              <Flex vertical gap={12}>
+                <Input placeholder='อีเมล' />
+              </Flex>
+              <Button className='bg-primary-blue text-white hover:bg-primary-blue px-4'>
+                รับอีเมล
+              </Button>
+            </div>
+          </div>
         </div>
-      )}
-      <div className='bg-primary-green px-10 py-10'>main content</div>
-      <div className='bg-primary-green px-10 py-10'>register content</div>
+      </div>
     </>
   )
 }
